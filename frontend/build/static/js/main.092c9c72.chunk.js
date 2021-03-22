9a48f94aad12c72a9756955e914f3d0a6abf0344
Iogsotot <ajustusa@gmail.com>
2021-03-22 10:38:12 +0300

    feat: add deploy branch

---
+++ frontend/build/static/js/main.092c9c72.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonprslang=this.webpackJsonprslang||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),s=t.n(c),r=t(3),a=t.n(r),i=(t(8),t(9),t(0));var o=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("header",{className:"App-header"})})};a.a.render(Object(i.jsx)(s.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.092c9c72.chunk.js.map
