9f10c561c783dba8649ef94c827c11ee097b5c66
bramov <alexey.abramov96@gmail.com>
2021-03-24 13:22:36 +0300

    feat: add basic structure for Memory Game

---
+++ frontend/src/components/Games/Memory/Memory.tsx
@@ -0,0 +1,12 @@
+import './memory.scss';
+import {MemoryProps} from "./Memory.model";
+import { useState, FC } from "react";
+
+const Memory: FC<MemoryProps> = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Memory;
