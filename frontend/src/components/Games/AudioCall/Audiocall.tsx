b5f79ca56878acac1aac80bf4b2343d3f2bff638
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2021-03-25 18:46:20 +0200

    feat: add game logic

--- frontend/src/components/Games/AudioCall/Audiocall.tsx
+++ frontend/src/components/Games/AudioCall/Audiocall.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 
 const Audiocall: FC = () => {
   const [count, setCount] = useState(0);
@@ -195,222 +195,35 @@ const Audiocall: FC = () => {
       textMeaningTranslate: 'Утка - маленькая водяная птица',
       wordTranslate: 'утка',
     },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4aa',
-      },
-      group: 0,
-      page: 0,
-      word: 'enjoy',
-      image: 'files/01_0011.jpg',
-      audio: 'files/01_0011.mp3',
-      audioMeaning: 'files/01_0011_meaning.mp3',
-      audioExample: 'files/01_0011_example.mp3',
-      textMeaning: 'To enjoy is to like something.',
-      textExample: 'The woman enjoys riding her bicycle.',
-      transcription: '[indʒɔ́i]',
-      __v: 0,
-      textExampleTranslate: 'Женщина любит кататься на велосипеде',
-      textMeaningTranslate: 'Наслаждаться значит любить что-то',
-      wordTranslate: 'наслаждаться',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4ab',
-      },
-      group: 0,
-      page: 0,
-      word: 'invite',
-      image: 'files/01_0012.jpg',
-      audio: 'files/01_0012.mp3',
-      audioMeaning: 'files/01_0012_meaning.mp3',
-      audioExample: 'files/01_0012_example.mp3',
-      textMeaning: 'To invite is to ask someone to come to a place or event.',
-      textExample: 'I will invite my friends to my birthday party.',
-      transcription: '[inváit]',
-      __v: 0,
-      textExampleTranslate: 'Я приглашаю своих друзей на мой день рождения',
-      textMeaningTranslate: 'Пригласить - это попросить кого-нибудь прийти на место или событие',
-      wordTranslate: 'пригласить',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4ac',
-      },
-      group: 0,
-      page: 0,
-      word: 'month',
-      image: 'files/01_0014.jpg',
-      audio: 'files/01_0014.mp3',
-      audioMeaning: 'files/01_0014_meaning.mp3',
-      audioExample: 'files/01_0014_example.mp3',
-      textMeaning: 'A month is one of 12 periods of time in one year.',
-      textExample: 'January is the first month of the year.',
-      transcription: '[mʌnθ]',
-      __v: 0,
-      textExampleTranslate: 'январь - первый месяц года',
-      textMeaningTranslate: 'Месяц - это один из 12 периодов времени в году',
-      wordTranslate: 'месяц',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4ad',
-      },
-      group: 0,
-      page: 0,
-      word: 'travel',
-      image: 'files/01_0015.jpg',
-      audio: 'files/01_0015.mp3',
-      audioMeaning: 'files/01_0015_meaning.mp3',
-      audioExample: 'files/01_0015_example.mp3',
-      textMeaning: 'To travel is to go to a faraway place on vacation or business.',
-      textExample: 'They will travel to Argentina this summer.',
-      transcription: '[trǽvəl]',
-      __v: 0,
-      textExampleTranslate: 'Этим летом они отправятся в Аргентину',
-      textMeaningTranslate: 'Путешествовать - это отправиться в далекое место на отдых или по делам',
-      wordTranslate: 'путешествовать',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4ae',
-      },
-      group: 0,
-      page: 0,
-      word: 'love',
-      image: 'files/01_0013.jpg',
-      audio: 'files/01_0013.mp3',
-      audioMeaning: 'files/01_0013_meaning.mp3',
-      audioExample: 'files/01_0013_example.mp3',
-      textMeaning: 'To love is to like something or someone a lot.',
-      textExample: 'I love my family very much.',
-      transcription: '[lʌv]',
-      __v: 0,
-      textExampleTranslate: 'Я очень люблю свою семью',
-      textMeaningTranslate: 'Любить значит любить что-то или кого-то много',
-      wordTranslate: 'любовь',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4af',
-      },
-      group: 0,
-      page: 0,
-      word: 'typical',
-      image: 'files/01_0016.jpg',
-      audio: 'files/01_0016.mp3',
-      audioMeaning: 'files/01_0016_meaning.mp3',
-      audioExample: 'files/01_0016_example.mp3',
-      textMeaning: 'If something is typical, it is normal, or something that usually happens.',
-      textExample: 'My typical breakfast is toast and eggs.',
-      transcription: '[típikəl]',
-      __v: 0,
-      textExampleTranslate: 'Мой типичный завтрак - тост и яйца',
-      textMeaningTranslate: 'Если что-то типичное, это нормально, или что-то, что обычно происходит',
-      wordTranslate: 'типичный',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4b0',
-      },
-      group: 0,
-      page: 0,
-      word: 'visit',
-      image: 'files/01_0017.jpg',
-      audio: 'files/01_0017.mp3',
-      audioMeaning: 'files/01_0017_meaning.mp3',
-      audioExample: 'files/01_0017_example.mp3',
-      textMeaning: 'To visit is to go and spend time in another place or see another person.',
-      textExample: 'She wants to visit her grandmother.',
-      transcription: '[vízit]',
-      __v: 0,
-      textExampleTranslate: 'Она хочет навестить свою бабушку',
-      textMeaningTranslate: 'Посетить - значит пойти и провести время в другом месте или увидеть другого человека',
-      wordTranslate: 'посещение',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4b1',
-      },
-      group: 0,
-      page: 0,
-      word: 'weather',
-      image: 'files/01_0018.jpg',
-      audio: 'files/01_0018.mp3',
-      audioMeaning: 'files/01_0018_meaning.mp3',
-      audioExample: 'files/01_0018_example.mp3',
-      textMeaning: 'Weather is the temperature and the state of the outdoors.',
-      textExample: 'Today’s weather is rainy and cloudy.',
-      transcription: '[weðər]',
-      __v: 0,
-      textExampleTranslate: 'Сегодня погода дождливая и облачная',
-      textMeaningTranslate: 'Погода это температура и состояние на улице',
-      wordTranslate: 'погода',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4b2',
-      },
-      group: 0,
-      page: 0,
-      word: 'wine',
-      image: 'files/01_0020.jpg',
-      audio: 'files/01_0020.mp3',
-      audioMeaning: 'files/01_0020_meaning.mp3',
-      audioExample: 'files/01_0020_example.mp3',
-      textMeaning: 'Wine is an alcoholic drink made from grapes.',
-      textExample: 'The store carried both red and white wine.',
-      transcription: '[wain]',
-      __v: 0,
-      textExampleTranslate: 'В магазине было красное и белое вино',
-      textMeaningTranslate: 'Вино - это алкогольный напиток из винограда',
-      wordTranslate: 'вино',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4b4',
-      },
-      group: 0,
-      page: 1,
-      word: 'adventure',
-      image: 'files/02_0021.jpg',
-      audio: 'files/02_0021.mp3',
-      audioMeaning: 'files/02_0021_meaning.mp3',
-      audioExample: 'files/02_0021_example.mp3',
-      textMeaning: 'An adventure is a fun or exciting thing that you do.',
-      textExample: 'Riding in the rough water was an adventure.',
-      transcription: '[ədvéntʃər]',
-      __v: 0,
-      textExampleTranslate: 'Езда в бурной воде была приключением',
-      textMeaningTranslate: 'Приключение - это забавная или захватывающая вещь, которую ты делаешь',
-      wordTranslate: 'приключение',
-    },
-    {
-      _id: {
-        $oid: '5e9f5ee35eb9e72bc21af4b3',
-      },
-      group: 0,
-      page: 0,
-      word: 'week',
-      image: 'files/01_0019.jpg',
-      audio: 'files/01_0019.mp3',
-      audioMeaning: 'files/01_0019_meaning.mp3',
-      audioExample: 'files/01_0019_example.mp3',
-      textMeaning: 'A week is a period of time that is seven days long.',
-      textExample: 'What are you doing next week?',
-      transcription: '[wiːk]',
-      __v: 0,
-      textExampleTranslate: 'Что ты делаешь на следующей неделе?',
-      textMeaningTranslate: 'Неделя - это период времени, который длится семь дней',
-      wordTranslate: 'неделя',
-    },
   ];
-  const [currentWord, setCurrentWord] = useState(words[0]);
+  const [currentWordNumber, setCurrentWordNumber] = useState(0);
+  const [currentWord, setCurrentWord] = useState(words[currentWordNumber]);
+
+  const stepAnswers = [currentWord.wordTranslate];
+  const fillStepAnswers = () => {
+    while (stepAnswers.length < 5) {
+      const randomWordNumber = Math.floor(Math.random() * words.length);
+      if (!stepAnswers.includes(words[randomWordNumber].wordTranslate)) {
+        stepAnswers.push(words[randomWordNumber].wordTranslate);
+      }
+    }
+  };
+
+  const shuffle = (array: string[]) => {
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
 
   const playAudio = () => {
     const wordAudio = new Audio(`https://rslang-2020q3.herokuapp.com/${currentWord.audio}`);
     wordAudio.play();
   };
+
+  useEffect(() => {
+    playAudio();
+  }, [currentWord]);
 
   const OpenCurrentWord = () => (
     <div>
@@ -424,22 +237,45 @@ const Audiocall: FC = () => {
       <button onClick={playAudio}>Play</button>
     </div>
   );
+  const nextStep = () => {
+    setCurrentWordNumber(currentWordNumber + 1);
+    setCurrentWord(words[currentWordNumber]);
+    fillStepAnswers();
+    shuffle(stepAnswers);
+    setCurrentView(false);
+  };
+
+  fillStepAnswers();
+  shuffle(stepAnswers);
+  if (currentWordNumber >= words.length) {
+    return <div>FINISH</div>;
+  }
   return (
     <div className="audiocall">
       <div className="current_word__box">
-        {currentView && <OpenCurrentWord/>}
-        {!currentView && <CloseCurrentWord/>}
+        {currentView && <OpenCurrentWord />}
+        {!currentView && <CloseCurrentWord />}
       </div>
       <div className="variants">
-        <button className="word_button">{currentWord.wordTranslate}</button>
-        <button className="word_button">{words[1].wordTranslate}</button>
-        <button className="word_button">{words[2].wordTranslate}</button>
-        <button className="word_button">{words[3].wordTranslate}</button>
-        <button className="word_button">{words[4].wordTranslate}</button>
+        <button className="word_button" onClick={() => setCurrentView(true)}>
+          {stepAnswers[0]}
+        </button>
+        <button className="word_button" onClick={() => setCurrentView(true)}>
+          {stepAnswers[1]}
+        </button>
+        <button className="word_button" onClick={() => setCurrentView(true)}>
+          {stepAnswers[2]}
+        </button>
+        <button className="word_button" onClick={() => setCurrentView(true)}>
+          {stepAnswers[3]}
+        </button>
+        <button className="word_button" onClick={() => setCurrentView(true)}>
+          {stepAnswers[4]}
+        </button>
       </div>
-      <button>Я не знаю</button>
+      {currentView && <button onClick={() => nextStep()}>Next</button>}
+      {!currentView && <button onClick={() => setCurrentView(true)}>Я не знаю</button>}
     </div>
   );
 };
-
 export default Audiocall;
