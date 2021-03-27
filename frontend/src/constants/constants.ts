5712e6ba8a84bbd2b9394d14902cf8aadc755daf
Iogsotot <ajustusa@gmail.com>
2021-03-27 20:58:00 +0300

    feat: add only build files

--- frontend/src/constants/constants.ts
+++
@@ -1,8 +0,0 @@
-export const FOOTER_COPYRIGHT = {
-  developers: [
-    { link: 'https://github.com/bramov', name: '@Bramov' },
-    { link: 'https://github.com/GregoryMoskalev', name: '@GregoryMoskalev' },
-    { link: 'https://github.com/johnneon', name: '@Johnneon' },
-    { link: 'https://github.com/Iogsotot', name: '@IogSotot' },
-  ],
-};
