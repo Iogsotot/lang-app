85e0059078959dd5063a1d96e1b1222b95c7c09a
Iogsotot <ajustusa@gmail.com>
2021-03-30 17:50:18 +0300

    WIP: game logic & timer

--- frontend/src/components/Games/Savannah/Savannah.tsx
+++ frontend/src/components/Games/Savannah/Savannah.tsx
@@ -1,20 +1,31 @@
-/* eslint-disable */
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+import React, { useCallback, useEffect, useState, FC, useRef } from 'react';
 import './savannah.scss';
-import { FC, useState, useEffect, useRef } from 'react';
 import { SavannahProps } from './Savannah.model';
 import { constants } from '../../../constants';
+import { Word } from '../../../models/word';
 
 const Savannah: FC<SavannahProps> = () => {
   const { WORD_GROUPS, API_BASE_URL } = constants;
-  // groupData должна приходить при переходе со словаря (это либо параметры Page & Group, либо слова из Store)
-  const [groupData, setGroupData] = useState(true);
-
   const [group, setGroup] = useState(0);
-  const [page, setPage] = useState(0);
-  const [currentWords, setCurrentWords] = useState([]);
+  const [currentWords, setCurrentWords] = useState<[Word] | []>([]);
 
-  async function fetchWords(group: number) {
-    const response = await fetch(`${API_BASE_URL}/words/all?group=${group}`, {
+  const WORDS = [0, 1, 2, 3];
+  const maxCount = 3;
+
+  const wordsChunk = (() => {
+    let wordsArr = [];
+    while (wordsArr.length < 4) {
+      let randomWordIndex = Math.floor(Math.random() * 600);
+      if (wordsArr.indexOf(randomWordIndex) === -1) wordsArr.push(randomWordIndex);
+    }
+    return wordsArr;
+  })();
+
+  async function fetchWords(wordsGroup: number) {
+    const response = await fetch(`${API_BASE_URL}/words/all?group=${wordsGroup}`, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
@@ -28,71 +39,91 @@ const Savannah: FC<SavannahProps> = () => {
     return response;
   }
 
-  const [time, setTime] = useState(false);
-  const [start, setStart] = useState(false);
-  let counter = 0;
-  const maxCount = 3;
-  const [startCountTimer, setStartCountTimer] = useState<number | null>(maxCount);
-
   useEffect(() => {
     async function fetchCurrentPageWords() {
       const currentPageWords = await fetchWords(group);
+      // console.log('-------------------------------------->');
+      console.log(currentPageWords);
       setCurrentWords(currentPageWords);
     }
     fetchCurrentPageWords();
-  }, [group, page]);
+  }, [group]);
 
-  useEffect(() => {
-    console.log(currentWords);
-  }, [currentWords]);
+  let lives = 5;
+  let isGetAnswer: boolean = false;
+  let currentWordClassNames = 'current-word';
 
-  useEffect(() => {
-    if (!time) {
-      return;
+  const soughtIndex = Math.floor(Math.random() * 4);
+
+  const [timer, setTimer] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [start, setStart] = useState(false);
+
+  function resolveAsWrongAnswer() {
+    if (isGetAnswer) {
+      lives -= 1;
     }
+    console.log('дурак что ли?');
+  }
 
-    const timerId = setTimeout(() => {
-      console.log('this is the end');
-      setTime(false);
-    }, 5150);
+  function resolveAsCorrectAnswer() {
+    isGetAnswer = true;
+    console.log('маладэц');
+  }
 
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [time]);
+  const resetGameRound = useCallback(() => {
+    setCounter(0);
+    setTimer(5);
+  }, []);
 
   useEffect(() => {
-    if (!start) {
+    if (!timer) {
       return;
     }
 
     const startTimerId = setInterval(() => {
-      console.log('tick ' + counter);
-      counter += 1;
-
-      setStartCountTimer(maxCount - counter);
-      if (counter === 3) {
-        setTime(true);
-        clearTimeout(startTimerId);
-        setStartCountTimer(null);
+      setCounter(counter + 1);
+      setTimer(timer - 1);
+      console.log(timer);
+      if (timer === 1) {
+        currentWordClassNames += ' start-anim';
+        resolveAsWrongAnswer();
+        resetGameRound();
+        // setCounter(0);
+        // setTimer(0);
+        clearInterval(startTimerId);
       }
     }, 1000);
 
     return () => {
       clearTimeout(startTimerId);
     };
-  }, [start]);
+  }, [counter, timer]);
 
-  let currentWordClassNames = 'current-word';
-  if (time) {
-    currentWordClassNames += ' start-anim';
+  const handleStart = useCallback(() => {
+    setTimer(5);
+    setStart(true);
+  }, []);
+
+  function checkPair(word: number) {
+    resetGameRound();
+    // setStart(false);
+    // setTime(false);
+    // setTime(true);
+    // setStartCountTimer(maxCount);
+    // counter = 0;
+    if (word === soughtIndex) {
+      resolveAsCorrectAnswer();
+      return;
+    }
+    resolveAsWrongAnswer();
   }
 
   function resetGame() {
     setStart(false);
-    setTime(false);
-    setStartCountTimer(maxCount);
-    counter = 0;
+    // setTime(false);
+    // setStartCountTimer(maxCount);
+    // counter = 0;
   }
 
   return (
@@ -104,7 +135,7 @@ const Savannah: FC<SavannahProps> = () => {
           resetGame();
         }}
       >
-        <i className="far fa-times"></i>
+        <i className="far fa-times" />
       </div>
 
       {!start && (
@@ -123,21 +154,24 @@ const Savannah: FC<SavannahProps> = () => {
                 onClick={() => {
                   setGroup(value);
                   setCurrentWords([]);
-                  setPage(0);
                 }}
               >
                 {key}
               </button>
             ))}
           </div>
-          <a href="#" className="btn button is-primary is-outlined" onClick={() => setStart(true)}>
+          {/* <button className="btn button is-primary is-outlined" onClick={handleStart} disabled={!!timer}> */}
+          <button className="btn button is-primary is-outlined" onClick={handleStart}>
             Начать игру!
-          </a>
+          </button>
+          {/* <div>counter: {counter}</div> */}
         </div>
       )}
       {start && (
         <div className="savannah-body">
           <div className="status-bar box">
+            <div>counter: {counter}</div>
+            {/* <button onClick={resetGameRound}>Перезапуск игры</button> */}
             <div className="lives">
               <i className="fas fa-heart"></i>
               <i className="fas fa-heart"></i>
@@ -148,16 +182,17 @@ const Savannah: FC<SavannahProps> = () => {
           </div>
 
           <div className="current-word__container title is-3 has-text-centered">
-            <div className="timer--start">{startCountTimer}</div>
-            <div className={currentWordClassNames}>{currentWords[0]['wordTranslate']}</div>
+            {/* <div className="timer--start">{timer}</div> */}
+            <div className={currentWordClassNames}>{currentWords[wordsChunk[soughtIndex]].wordTranslate}</div>
           </div>
 
           <div className="answer-variants box">
             <div className="wrapper">
-              <a className="button is-info is-light">{currentWords[1]['word']}</a>
-              <a className="button is-info is-light">{currentWords[0]['word']}</a>
-              <a className="button is-info is-light">{currentWords[3]['word']}</a>
-              <a className="button is-info is-light">{currentWords[4]['word']}</a>
+              {WORDS.map(word => (
+                <div className="button is-info is-light" onClick={() => checkPair(word)} key={word}>
+                  {currentWords[wordsChunk[word]].word}
+                </div>
+              ))}
             </div>
           </div>
         </div>
