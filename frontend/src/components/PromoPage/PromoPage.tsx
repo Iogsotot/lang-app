673507f73df6cf343cb4bf565a5aa35898fbaa5d
Iogsotot <ajustusa@gmail.com>
2021-03-31 15:36:55 +0300

    feat: add promo for PromoPage

--- frontend/src/components/PromoPage/PromoPage.tsx
+++ frontend/src/components/PromoPage/PromoPage.tsx
@@ -1,11 +1,26 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import './promoPage.scss';
+import { PromoPageProps } from './PromoPage.model';
 
-const PromoPage: FC = () => (
-  <>
-    <Link to="/textbook/0/0">TextBook page</Link>
-    <Link to="/savannah">Savannah</Link>
-  </>
+import Menu from '../Menu';
+
+const PromoPage: FC<PromoPageProps> = () => (
+  <main>
+    <section className="promo">
+      <div className="wrapper">
+        <div className="info_block">
+          <h1 className="title title--bigest">Вдохновляющий и продающий текст! мы такие классные!</h1>
+          <p className="content">Тут тоже о том, как классно в нашей аппе учить англ</p>
+          <Link to="/auth" className="btn">
+            Войти
+          </Link>
+        </div>
+      </div>
+    </section>
+    {/* <Link to="/textbook/0/0">TextBook page</Link>
+    <Link to="/savannah">Savannah</Link> */}
+  </main>
 );
 
 export default PromoPage;
