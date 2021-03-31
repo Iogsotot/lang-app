98bf72df4f37246b869599ce8568d0118d44e681
Iogsotot <ajustusa@gmail.com>
2021-03-31 20:10:37 +0300

    feat: add team section in PromoPage

--- frontend/src/components/PromoPage/PromoPage.tsx
+++ frontend/src/components/PromoPage/PromoPage.tsx
@@ -28,6 +28,7 @@ const PromoPage: FC<PromoPageProps> = () => (
         className="video-player"
       />
     </section>
+
     <section className="features" id="features">
       <h2>
         Что можно делать в <b>RS lang</b>
@@ -59,6 +60,65 @@ const PromoPage: FC<PromoPageProps> = () => (
             Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
             aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae
           </p>
+        </div>
+      </div>
+    </section>
+
+    <section className="team">
+      <h3 className="title">Наша команда</h3>
+      <h3 className="subtitle">
+        У нас огромная команда из высоквалифицированных лоу-перформеров. Вот тут ниже наши рожицы и кто что делал
+      </h3>
+      <div className="team__group">
+        <div className="teammates">
+          <div className="bramov">
+            <p className="duty">Max - 10+ years of tech recruiting for VC funded startups</p>
+            <div className="teammate__info">
+              <img src="https://avatars.githubusercontent.com/u/54726292?v=4" alt="Bramov" />
+              <div className="name">Bramov</div>
+            </div>
+          </div>
+
+          <div className="ainuAnna">
+            <p className="duty">Max - 10+ years of tech recruiting for VC funded startups</p>
+            <div className="teammate__info">
+              <img src="https://avatars.githubusercontent.com/u/65246069?v=4" alt="Ann Tereshko" />
+              <div className="name">Ann Tereshko</div>
+            </div>
+          </div>
+
+          <div className="svetlana-tyshkevich">
+            <p className="duty">Max - 10+ years of tech recruiting for VC funded startups</p>
+            <div className="teammate__info">
+              <img src="https://avatars.githubusercontent.com/u/70635819?v=4" alt="Svetlana Tyshkevich" />
+              <div className="name">Svetlana Tyshkevich</div>
+            </div>
+          </div>
+
+          <div className="gregoryMoskalev">
+            <p className="duty">Max - 10+ years of tech recruiting for VC funded startups</p>
+            <div className="teammate__info">
+              <img src="https://avatars.githubusercontent.com/u/62020954?v=4" alt="Gregory Moskalev" />
+              <div className="name">Gregory Moskalev</div>
+            </div>
+          </div>
+
+          <div className="johnneon">
+            <p className="duty">Max - 10+ years of tech recruiting for VC funded startups</p>
+            <div className="teammate__info">
+              <img src="https://avatars.githubusercontent.com/u/53760291?v=4" alt="Efimovich Evgenii" />
+              <div className="name">Efimovich Evgenii</div>
+            </div>
+          </div>
+
+          <div className="iogsotot">
+            <p className="duty">Max - 10+ years of tech recruiting for VC funded startups</p>
+            <div className="teammate__info">
+              <img src="https://avatars.githubusercontent.com/u/50149163?v=4" alt="Anna Justus" />
+              <div className="name">Anna Justus</div>
+            </div>
+          </div>
+          <img src="./images/team_faces.png" alt="team" className="center" />
         </div>
       </div>
     </section>
