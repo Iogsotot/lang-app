57d9bb0238c3d3f3793088e14ba9f6685cd77038
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2021-04-02 00:34:54 +0300

    feat: add scrollbar

--- frontend/src/components/Games/AudioCall/Finish/Finish.tsx
+++ frontend/src/components/Games/AudioCall/Finish/Finish.tsx
@@ -1,51 +1,51 @@
 import { FC } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 import Word from '../Audiocall.model';
 import { FinishProps } from './Finish.model';
 
 const Finish: FC<FinishProps> = ({ correctAnswers, wrongAnswers, playAudio }) => {
-  const CorrectWords = () => (
+  const finishList = (list: Word[]) => (
     <div>
-      {correctAnswers.map(word =>
+      {list.map((word: Word) => (
         <div className="finish__words-list__row">
-          <button onClick={() => playAudio(word)} key={word.word} className="audiocall__volume ">
+          <button onClick={() => playAudio(word)} key={word.word} className="audiocall__volume volume-button">
             <i className="fas fa-volume-up"></i>
           </button>
-          <span>{word.word.toUpperCase()}</span>
-          <span>{` - ${word.wordTranslate}`}</span>
-        </div>)}
+          <span>{`${word.word.toUpperCase()} - ${word.wordTranslate}`}</span>
+        </div>
+      ))}
     </div>
   );
+
+  const CorrectList = () => finishList(correctAnswers);
+
+  const WrongList = () => finishList(wrongAnswers);
 
   return (
     <div className="audiocall">
       <div className="box finish">
         <div>Отличный результат!</div>
         <div>{`${correctAnswers.length} изучено, ${wrongAnswers.length} на изучении`}</div>
-        <div className="finish__words-list">
-          <div>
-            <div>
-              <span>Знаю</span>
-              <span>{correctAnswers.length}</span>
-            </div>
-            <CorrectWords />
-          </div>
 
-          <div>
+        <Scrollbars >
+          <div className="finish__words-list">
             <div>
-              <span>Сложно</span>
-              <span>{wrongAnswers.length}</span>
-            </div>
-            {wrongAnswers.map(word => (
-              <div className="finish__words-list__row">
-                <button onClick={() => playAudio(word)} key={word.word} className="audiocall__volume ">
-                  <i className="fas fa-volume-up"></i>
-                </button>
-                <span>{word.word.toUpperCase()}</span>
-                <span>{` - ${word.wordTranslate}`}</span>
+              <div>
+                <span>Знаю</span>
+                <span className="finish__akcent-number">{correctAnswers.length}</span>
               </div>
-            ))}
+              <CorrectList />
+            </div>
+
+            <div>
+              <div>
+                <span>Сложно</span>
+                <span className="finish__akcent-number">{wrongAnswers.length}</span>
+              </div>
+              <WrongList />
+            </div>
           </div>
-        </div>
+        </Scrollbars>
       </div>
     </div>
   );
