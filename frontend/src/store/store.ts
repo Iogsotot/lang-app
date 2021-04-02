e8ef54782cb5c26c1e50608f1d492f8e395974a0
Iogsotot <ajustusa@gmail.com>
2021-04-02 17:34:15 +0300

    feat: store subscribe () => LS

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,10 +2,18 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
+// import { initialState } from './reducers/words'
+
+const preloadedState = localStorage.reduxState ? JSON.parse(localStorage.reduxState) : rootReducer;
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(
-  rootReducer,
+const store = createStore(
+  // rootReducer,
+  preloadedState,
   composeEnhancers(applyMiddleware(thunk)),
 );
+
+store.subscribe(() => localStorage.reduxState = JSON.stringify(store.getState()));
+
+export default store;
