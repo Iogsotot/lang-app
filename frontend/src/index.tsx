337a51cb743180077bd2ef90d62c00d705d896e4
Iogsotot <ajustusa@gmail.com>
2021-04-03 11:00:26 +0300

    fix: fonts imports

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,9 @@ import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'typeface-roboto';
+import 'typeface-roboto-mono';
+import 'typeface-rubik';
 
 ReactDOM.render(
   <React.StrictMode>
