0ed36b079b7006ec531f16b2d04e7316b2b48b24
bramov <alexey.abramov96@gmail.com>
2021-04-04 18:49:13 +0300

    feat: add dragndrop functionalty

---
+++ frontend/src/components/Games/Puzzle/Puzzle.model.ts
@@ -0,0 +1,13 @@
+import { Word } from '../../../models/word';
+
+export interface PuzzleProps {
+  smth?: any;
+}
+export interface VariantsProps {
+  items: Word[],
+}
+export interface PhraseProps {
+  item: Word | undefined,
+  phrase: string | null,
+  word: string | null,
+}
---
+++ frontend/src/components/Games/Puzzle/Variants/Variants.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+import { Word } from '../../../../models/word';
+import { VariantsProps } from '../Puzzle.model';
+
+const grid = 8;
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 ${grid}px 0 0`,
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver: boolean) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  display: 'flex',
+  padding: grid,
+  overflow: 'auto',
+});
+
+const Variants = (props: VariantsProps) => {
+  const { items } = props;
+  return (
+    <Droppable droppableId="variants" direction="horizontal">
+      {(provided, snapshot) => (
+        <div ref={provided.innerRef} className="cards-wrapper" style={getListStyle(snapshot.isDraggingOver)}>
+          {items && items.map((item: Word, index: number) => (
+            <Draggable key={item.id} draggableId={item.id} index={index}>
+              {(provided2, snapshot2) => (
+                <div
+                  ref={provided2.innerRef}
+                  {...provided2.draggableProps}
+                  {...provided2.dragHandleProps}
+                  className="card"
+                  style={getItemStyle(
+                    snapshot2.isDragging,
+                    provided2.draggableProps.style,
+                  )}
+                >
+                  {item.word}
+                </div>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+};
+
+export default Variants;
