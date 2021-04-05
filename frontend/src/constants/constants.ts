a3ac571efd88d30ab19543ed5e071792fc829c8b
Iogsotot <ajustusa@gmail.com>
2021-04-05 14:15:04 +0300

    merge

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -24,9 +24,32 @@ export const WORD_GROUPS = {
   Legendary: 5,
 };
 
+export const SPRINT = {
+  gameDuration: 60,
+  timerColor: '#00d1b2',
+  timerSize: 80,
+  timerStrokeWidth: 3,
+  colorOnCorrectAnswer: '141, 71%, 48%', // hsl(141, 71%, 48%)
+  colorOnWrongAnswer: '348, 100%, 61%', // hsl(348, 100%, 61%)
+  wrongBtnText: 'Wrong',
+  correctBtnText: 'Correct',
+  wordsAmount: 10,
+  basicPoints: 10,
+  maxModificator: 4,
+  maxStreak: 3,
+};
+
 export const SPRINT_MODAL_ON_CLOSE = {
   modalTitle: "You haven't finished this training!",
   modalSubTitle: "If you close the training, you'll lose your results",
   sumbitCloseBtnText: 'Close training',
   canselModalBtn: 'Cancel',
+};
+
+export const SPRINT_GET_READY = {
+  title: 'Get Ready',
+  timerColor: '#00d1b2',
+  timerDuration: 5,
+  timerStrokeWidth: 3,
+  timerSize: 80,
 };
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,6 +5,7 @@ import PromoPage from './components/PromoPage';
 import AuthPage from './components/AuthPage';
 import Savannah from './components/Games/Savannah';
 import Audiocall from './components/Games/AudioCall';
+import Sprint from './components/Games/Sprint';
 import { useTypedSelector } from './hooks/useTypedSelector';
 
 export const Router: FC = () => {
@@ -23,6 +24,10 @@ export const Router: FC = () => {
 
         <Route path="/savannah/" exact>
           <Savannah />
+        </Route>
+
+        <Route path="/sprint/" exact>
+          <Sprint />
         </Route>
 
         <Route path="/audiocall/">
@@ -46,6 +51,10 @@ export const Router: FC = () => {
 
       <Route path="/savannah/" exact>
         <Savannah />
+      </Route>
+
+      <Route path="/sprint/" exact>
+        <Sprint />
       </Route>
 
       <Route path="/audiocall/">
