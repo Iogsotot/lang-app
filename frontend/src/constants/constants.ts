fac52af9cf940fcf1345de13698ccd0157615e5e
Gregory Moskalev <gregory.moskalev@gmail.com>
2021-04-05 23:12:13 +0300

    feat: localize sprint

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -31,8 +31,8 @@ export const SPRINT = {
   timerStrokeWidth: 3,
   colorOnCorrectAnswer: '141, 71%, 48%', // hsl(141, 71%, 48%)
   colorOnWrongAnswer: '348, 100%, 61%', // hsl(348, 100%, 61%)
-  wrongBtnText: 'Wrong',
-  correctBtnText: 'Correct',
+  wrongBtnText: 'Неверно',
+  correctBtnText: 'Верно',
   wordsAmount: 10,
   basicPoints: 10,
   maxModificator: 4,
@@ -40,14 +40,14 @@ export const SPRINT = {
 };
 
 export const SPRINT_MODAL_ON_CLOSE = {
-  modalTitle: "You haven't finished this training!",
-  modalSubTitle: "If you close the training, you'll lose your results",
-  sumbitCloseBtnText: 'Close training',
-  canselModalBtn: 'Cancel',
+  modalTitle: 'Тренировка не закончена!',
+  modalSubTitle: 'Если вы закроете тренировку, ваши результаты обнулятся',
+  sumbitCloseBtnText: 'Закрыть тренировку',
+  canselModalBtn: 'Отмена',
 };
 
 export const SPRINT_GET_READY = {
-  title: 'Get Ready',
+  title: 'Приготовьтесь',
   timerColor: '#00d1b2',
   timerDuration: 5,
   timerStrokeWidth: 3,
