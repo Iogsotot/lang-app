c0adff2fb0df55c028ff19ebab48eb8062b28582
Iogsotot <ajustusa@gmail.com>
2021-04-05 10:19:22 +0300

    fix: auth styles & menu

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -121,45 +121,6 @@ const Menu: FC = () => {
           </div>
         </div>
       </div>
-      <nav>
-        <ul className="menu__list">
-          <li className="menu__item">
-            <Link to="/textbook/0/0">
-              <img src="./images/textbook_icon.png" alt="textbook" className="icon" />
-            </Link>
-          </li>
-          <li className="menu__item">
-            <Link to="/stats">
-              <img src="./images/stats_icon.png" alt="stats" className="icon" />
-            </Link>
-          </li>
-          <li className="menu__item">
-            <Link to="/sprint">
-              <img src="./images/game_icon.png" alt="sprint" className="icon" />
-            </Link>
-          </li>
-          <li className="menu__item">
-            <Link to="/savannah">
-              <img src="./images/game_icon.png" alt="savannah" className="icon" />
-            </Link>
-          </li>
-          <li className="menu__item">
-            <Link to="/find-pair">
-              <img src="./images/game_icon.png" alt="find-pair" className="icon" />
-            </Link>
-          </li>
-          <li className="menu__item">
-            <Link to="/audiocall">
-              <img src="./images/game_icon.png" alt="audiocall" className="icon" />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="down-group">
-        <Link to="/audiocall">
-          <img src="./images/settings_icon.png" alt="setting" className="icon" />
-        </Link>
-      </div>
     </aside>
   );
 };
